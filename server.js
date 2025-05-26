2e8daa398fa5678355ebfb053b7d71b67b78797b
Omar Tegar <omarahmed_3@yahoo.com>
2025-05-26 20:45:47 +0300

    chat_project

---
+++ server.js
@@ -0,0 +1,55 @@
+const { error } = require('console');
+
+require('dotenv').config();
+const
+    express = require('express'),
+    app = express(),
+    pg = require('pg'),
+    PORT = 3000,
+    DATABASE = process.env.DATABASE,
+    client = new pg.Client(DATABASE);
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(__dirname + '/front-end/')); // to see the files like css
+
+app.listen(PORT , () =>  `listening on port ${PORT}`);
+
+
+app.get('/postMessage' , (req , res) => {
+    client.query('select * from messagestable;').then(myTableResult => {
+        res.json(myTableResult.rows);
+    })
+} );
+
+app.post('/sendMessage' , ( req , res ) => {
+    const inputsTaken = [req.body.username , req.body.chatMessage];
+    const sqlSentence = `insert into messagestable (name , message) values ($1, $2) ;`;
+    client.query(sqlSentence , inputsTaken).then( () => {
+        console.log("new data added to table !!");
+    })
+    .then(() => {
+        console.log("Page reloaded!");
+        res.redirect('/');
+    })
+})
+
+app.post('/deleteAll', (req , res) =>{
+    console.log("Deleting messagestable !")
+    client.query("drop table messagestable;")
+    .then( () => {
+        console.log("Creating messagestable again!");
+        client.query('create table messagestable (name varchar(255) , message varchar(255)) ; ')
+        .then( () => {
+            console.log("Table created successfully !");
+            res.redirect('/');
+        })
+    })
+})
+
+
+client.connect().then( function(){
+    console.log("Connected successfully to Database : railway");
+})
+
